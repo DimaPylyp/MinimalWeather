839c4dd00e703422f0f2c3b88dc2541880304bbf
Dmytro Pylypenko <pylyp09@gmail.com>
2020-03-04 15:27:44 +0100

    Initial Commit

---
+++ MinimalWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MinimalWeather
+//
+//  Created by DiMa on 04/03/2020.
+//  Copyright © 2020 DiMa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
